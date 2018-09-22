8b602161debbd77cb1e427806adb997e3088cf9d
root <root@localhost.localdomain>
2018-09-22 13:52:46 +0200

    re-added ox-twbs

---
+++ .emacs.d/elpa/fontify-face-20180420.1624/fontify-face-autoloads.el
@@ -0,0 +1,22 @@
+;;; fontify-face-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "fontify-face" "fontify-face.el" (23461 49470
+;;;;;;  219197 610000))
+;;; Generated autoloads from fontify-face.el
+
+(autoload 'fontify-face-mode "fontify-face" "\
+Fontify symbols representing faces with that face.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; fontify-face-autoloads.el ends here
